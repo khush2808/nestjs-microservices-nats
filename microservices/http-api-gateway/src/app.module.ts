80d5b912c09de39c5f142b99d88cd8186eeba521
Khush Shah <khushshah675@gmail.com>
2025-06-07 02:23:33 +0530

    docker setup.

--- microservices/http-api-gateway/src/app.module.ts
+++ microservices/http-api-gateway/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Transport } from '@nestjs/microservices';
+import { ClientsModule } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NATS_SERVICE',
+        transport: Transport.NATS,
+        options: {
+          servers: ['nats://nats'],
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ microservices/users-microservice/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+
+import { ClientsModule } from '@nestjs/microservices';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
